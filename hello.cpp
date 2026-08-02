3d00b592e6817573a4d1567419896bb08ad6f476
Hakeem sebut <bolaji9626@gmail.com>
2026-08-02 14:11:00 +0100

    add a comment to hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "I just wrote C++\n";
+    std::cout << "I just wrote C++ and working on an amazing project\n"; // i am getting good at c++
     return 0;
 }
