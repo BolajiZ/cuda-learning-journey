544b42a8f7a70bba550ec6f1130aa8b379771e4c
Hakeem sebut <bolaji9626@gmail.com>
2026-08-02 16:36:44 +0100

    modified capstone.cpp project

--- capstone.cpp
+++ capstone.cpp
@@ -1,49 +1,45 @@
 #include <iostream>
 
+
 struct DynArray {
     int* data;
     int size;
     int capacity;
 
+
 };
 
 void push_back(DynArray& arr, int value) {
-    if (arr.size == arr.capacity) {
-        int newCapacity = arr.capacity * 2; // since we have a full capacity we double the existing capacity
-        int* newData = new int[newCapacity]; // ask the heap for the new data
-        for (int i = 0; i < arr.size; i++) {
+    if(arr.size == arr.capacity){
+        int newCapacity = arr.capacity * 2;
+        int* newData = new int[newCapacity];
+        for (int i = 0; i < arr.size; i++){
             newData[i] = arr.data[i];
-
         }
-        // right after the for-loop that copies:
-        std::cout << "after grow: box0=" << newData[0] 
-                  << " box1=" << newData[1] << "\n";
         delete[] arr.data;
         arr.data = newData;
         arr.capacity = newCapacity;
-    }
 
+    }
     arr.data[arr.size] = value;
     arr.size = arr.size + 1;
-
 }
-int main(){
+
+int main() {
     DynArray arr;
     arr.capacity = 2;
     arr.size = 0;
     arr.data = new int[arr.capacity];
 
     push_back(arr, 10);
-    push_back(arr, 20);
+    push_back(arr, 20); // full here we trigger the grow in it
     push_back(arr, 30);
     push_back(arr, 40);
 
     std::cout << "size: " << arr.size << ", capacity: " << arr.capacity << "\n";
     for (int i = 0; i < arr.size; i++) {
         std::cout << arr.data[i] << " ";
-
     }
-
     std::cout << "\n";
 
     delete[] arr.data;
