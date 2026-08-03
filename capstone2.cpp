8f7cd3cec97bde247bf568fd0b4d2375f54db388
Hakeem sebut <bolaji9626@gmail.com>
2026-08-03 21:29:00 +0100

    capstone 2 complete: matrix with get, set, and print

--- capstone2.cpp
+++ capstone2.cpp
@@ -20,6 +20,16 @@ void set(Matrix& m, int row, int col, int value) {
     m.data[index] = value;
 }
 
+void print(Matrix&m) {
+    for (int row = 0; row < m.rows; row++) {
+        for (int col = 0; col < m.cols; col++) {
+            std::cout << get(m, row, col) << " ";
+
+        }
+        std::cout << "\n";
+    }
+}
+
 int main() {
     Matrix m;
     m.rows = 2;
@@ -29,9 +39,7 @@ int main() {
     set(m, 0, 0, 1);   set(m, 0, 1, 2);   set(m, 0, 2, 3);
     set(m, 1, 0, 4);   set(m, 1, 1, 5);   set(m, 1, 2, 6);
     
-    // read out a few values using get()
-    std::cout << "(0,1) = " << get(m, 0, 1) << "\n";
-    std::cout << "(1,2) = " << get(m, 1, 2) << "\n";
+    print(m);
 
     delete[] m.data;
     return 0;
